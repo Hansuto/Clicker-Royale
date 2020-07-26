831abd2ba1ae8149c275826d20cf2c5c9d5f996a
RobDahl <42755777+RobDahl@users.noreply.github.com>
2020-07-26 13:45:52 -0400

    Semantic UI
    Went from Material UI to Semantic UI. Added header, button and leaderboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Grid, Segment, Button } from 'semantic-ui-react';
+import Head from './components/head';
+import Leaderboard from './components/leaderboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameInProgress: false,
+      timeLeft: 59,
+      leaderboard: [
+        {
+          name: 'Rob',
+          score: 420,
+        },
+        {
+          name: 'Chris',
+          score: 69,
+        },
+      ],
+      myScore: 420,
+    }
+  }
+
+  render() {
+    const { gameInProgress, timeLeft, myScore, leaderboard } = this.state;
+    return (
+      <Segment className="App">
+        <Grid centered className="flex fillHeight">
+          <Grid.Row columns='1' className="flexShrink">
+            <Head playing={gameInProgress} time={timeLeft} />
+          </Grid.Row>
+          <Grid.Row columns='2' className="flexGrow">
+            <Grid.Column width='10'>
+              <Button fluid color='pink' size='massive' className="fillHeight">{myScore}</Button>
+            </Grid.Column>
+            <Grid.Column width='6'>
+              <Leaderboard data={leaderboard} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    );
+  }
 }
 
 export default App;
