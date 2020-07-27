bbda2883c2ec800670d08bb527d61abce9b791ae
Christopher Louis Taliaferro <guitartaliaferro@knights.ucf.edu>
2020-07-26 23:56:23 -0400

    Changed Game length

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var io = require('socket.io').listen(8123);
-var time = 60;
+var time = 30;
 var gameInProgress = true;
 var lastWinner = '';
 var leaderboard = [];
@@ -7,16 +7,18 @@ var leaderboard = [];
 io.on('connection', function(socket) {
   socket.on('click', function(data) {
 		
+		
 		var parsedData = JSON.parse(data)
 		
 		console.log(parsedData)
 		
 		let obj = leaderboard.find((o, i) => {
 			if (o.name === parsedData.name) {
-					if(gameInProgress)
+					if(gameInProgress) {
 						var newScore = o.score + parsedData.increment
 						if (newScore <= 0 ) newScore = 0;
 						leaderboard[i] = { name: o.name, score: newScore };
+					}
 					return true;
 			}
 		});
@@ -60,6 +62,8 @@ setInterval(function(){
 setInterval(function(){
 		time -= 1;
 		
+		if (time <= 0) time = 0;
+		
 		if (time <= 0 && leaderboard.length != 0) {
 			if (gameInProgress) {
 					var winner = leaderboard.reduce((p, c) => p.score > c.score ? p : c);
@@ -71,7 +75,7 @@ setInterval(function(){
 					leaderboard = []
 			}
 			gameInProgress = !gameInProgress			
-			time = 60;
+			time = 30;
 		}
 		
 		//console.log(leaderboard);
